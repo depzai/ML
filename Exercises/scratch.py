2a9d0683a70d72993fc752bb2e9624194aa778e4
depzai <32101016+depzai@users.noreply.github.com>
2017-11-08 09:55:30 -0500

    1107

--- Exercises/scratch.py
+++ Exercises/scratch.py
@@ -1,20 +1,7 @@
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use('ggplot')
 import numpy as np
-import sklearn
-from sklearn.cluster import KMeans
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.preprocessing import Imputer
-from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import FeatureUnion
-import pandas as pd
-from pandas.plotting import scatter_matrix
+from sklearn.metrics import accuracy_score
+y_pred = [0, 2, 2, 3]
+y_true = [0, 1, 2, 3]
+print(accuracy_score(y_true, y_pred))
 
-housing = pd.read_csv("housing.csv")
-print(housing.head())
 
