ea1fb26d3090efe10c7238c130ebc3b33f548951
depzai <32101016+depzai@users.noreply.github.com>
2017-11-15 17:24:08 -0500

    1114

--- Clustering/Meanshiftfromscratch.py
+++ Clustering/Meanshiftfromscratch.py
@@ -6,7 +6,10 @@ from sklearn.cluster import MeanShift
 
 X = np.array([[1, 2],
               [1.5, 1.8],
-              [9, 10 ],
+              [4, 10 ],
+              [5, 10 ],
+              [4.5, 11 ],
+              [9, 10 ],              
               [8, 10],
               [1, 0.6],
               [9,11],
@@ -14,8 +17,8 @@ X = np.array([[1, 2],
               [10,2],
               [9,3],])
 
-plt.scatter(X[:,0], X[:,1], s=150, color = 'b')
-plt.show()
+# plt.scatter(X[:,0], X[:,1], s=150, color = 'b')
+# plt.show()
 
 colors = 10*["g","r","c","b","k"]
 
@@ -23,7 +26,7 @@ colors = 10*["g","r","c","b","k"]
 
 
 # using sklearn
-clf = MeanShift()
+clf = MeanShift(bandwidth = 2)
 clf.fit(X)
 
 labels = clf.labels_
@@ -31,12 +34,11 @@ n_clusters_ = len(np.unique(labels))
 print("Number of estimated clusters:", n_clusters_)
 
 centroids = clf.cluster_centers_
-
 print(centroids)
 
-# plt.scatter(X[:,0], X[:,1], s=150)
+plt.scatter(X[:,0], X[:,1], s=150)
 
-for c in centroids:
+for c in range(len(centroids)):
      plt.scatter(centroids[c][0], centroids[c][1], color='k', marker='*', s=150)
 
 plt.show()
