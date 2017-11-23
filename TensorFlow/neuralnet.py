fd07ace4d51f079110379bcd287c52c9670ef079
depzai <32101016+depzai@users.noreply.github.com>
2017-11-22 22:02:39 -0500

    1123

--- TensorFlow/neuralnet.py
+++ TensorFlow/neuralnet.py
@@ -39,27 +39,50 @@ for 10 classes:
 2 = [0,0,1,0,0,0,0,0,0,0]
 '''
 #3 hidden layers (deep neural network so more than one hidden layer)
+# number of nodes can change
 n_nodes_hl1 = 500
 n_nodes_hl2 = 500
 n_nodes_hl3 = 500
 
 n_classes = 10
-batch_size = 100
+
+batch_size = 100 # goes through batches of 100 features at a time
 
 
+# height by width
+x = tf.placeholder('float',[None, 784]) # 784 values (28 by 28 pixels), doesn't have to be a mtrix, so no other dims (None)
+y = tf.placeholder('float') # labels
+
+# input data * weights + bias is the formula, bias in case all inputs are 0
+
+def neural_network_model(data):
+	hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784,n_nodes_hl1])),
+	'biases':tf.variable(tf.random_normal(n_nodes_hl1))}
+	hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1,n_nodes_hl2])),
+	'biases':tf.variable(tf.random_normal(n_nodes_hl2))}
+	hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2,n_nodes_hl3])),
+	'biases':tf.variable(tf.random_normal(n_nodes_hl3))}
+	output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3,n_nodes_hl1])),
+	'biases':tf.variable(tf.random_normal([n_classes]))}
 
 
+	# for each layer: input data * weights + bias is the formula, bias in case all inputs are 0
 
+	l1 = tf.add(tf.matmul(data, hidden_1_layer['weights'])) + hidden_1_layer['biases']
+	#activation function (sigmoid to define neuron's value)
+	l1 = tf.nn.relu(l1)
 
+	l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights'])) + hidden_2_layer['biases']
+	#activation function (sigmoid to define neuron's value)
+	l2 = tf.nn.relu(l2)
 
+	l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights'])) + hidden_3_layer['biases']
+	#activation function (sigmoid to define neuron's value)
+	l3 = tf.nn.relu(l3)
 
+	output = tf.add(tf.matmul(l3, output_layer['weights'])) + output_layer['biases']
 
-
-
-
-
-
-
+	return output
 
 
 
